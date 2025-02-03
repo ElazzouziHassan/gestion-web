5e060bcf004b674499357d173620cd1dfab3e327
Wizardy <wizardhassan99@gmail.com>
2025-02-03 15:32:57 +0100

    commit

--- lib/env.ts
+++ lib/env.ts
@@ -1,5 +1,5 @@
 export function validateEnv() {
-  const requiredEnvVars = ["URI", "JWT_SECRET"]
+  const requiredEnvVars = ["MONGODB_URI", "JWT_SECRET"]
   for (const envVar of requiredEnvVars) {
     if (!process.env[envVar]) {
       throw new Error(`${envVar} is not defined in the environment variables`)
